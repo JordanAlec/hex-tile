a4429bf7cf8de94bfdaaa32732de012eabea6158
JordanAlec <57456980+JordanAlec@users.noreply.github.com>
2026-01-09 15:02:00 +0000

    feat: scaffold solution and projects

---
+++ README.md
@@ -0,0 +1,2 @@
+# HexTile
+HexTile lets you control your HX Stomp foot switches when you can't get near them!
---
+++ src/DesktopApp/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace DesktopApp
+{
+    public partial class App : Application
+    {
+    }
+
+}
